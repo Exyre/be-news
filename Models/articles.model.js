33c0291781297b63e88b74454902fdea0d9e2f4e
Exyre <munirv97@gmail.com>
2025-03-12 12:19:26 +0000

    Completed ticket-5: Get article comments

--- Models/articles.model.js
+++ Models/articles.model.js
@@ -3,7 +3,7 @@ const db = require("../db/connection")
 function fetchArticleById(article_id) {
     return db.query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
         .then(({ rows }) => {
-            if (!rows.length) {
+            if (rows.length === 0) {
                 return Promise.reject({ status: 404, msg: "Article not found" });
             }
             return rows[0];
