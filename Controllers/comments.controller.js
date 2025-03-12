b14df2997ad5f6aa01965464f93e80dc04a291a0
Exyre <munirv97@gmail.com>
2025-03-12 14:04:31 +0000

    Implement POST /api/articles/:article_id/comments with error handling and tests

--- Controllers/comments.controller.js
+++ Controllers/comments.controller.js
@@ -1,4 +1,4 @@
-const { fetchCommentsByArticleId } = require("../Models/comments.model")
+const { fetchCommentsByArticleId, insertCommentByArticleId } = require("../Models/comments.model")
 
 function getCommentsByArticleId(req, res, next) {
     const { article_id } = req.params;
@@ -10,4 +10,27 @@ function getCommentsByArticleId(req, res, next) {
         .catch(next);
 }
 
-module.exports = { getCommentsByArticleId };
+function postCommentByArticleId(req, res, next) {
+    const { article_id } = req.params
+    const { username, body } = req.body
+
+    if (!username || !body) {
+        return res.status(400).send({ msg: "Bad request - missing required fields" });
+    }
+
+    insertCommentByArticleId(article_id, username, body)
+        .then((comment) => {
+            res.status(201).send({ comment });
+        })
+        .catch((err) => {
+            if (err.code === "23503") {
+                res.status(404).send({ msg: "Article not found or user does not exist" });
+            } else if (err.code === "22P02") {
+                res.status(400).send({ msg: "Invalid article ID" })
+            } else {
+                next(err)
+            }
+        })
+}
+
+module.exports = { getCommentsByArticleId, postCommentByArticleId};
--- Models/comments.model.js
+++ Models/comments.model.js
@@ -1,27 +1,28 @@
 const db = require("../db/connection")
 
 function fetchCommentsByArticleId(article_id) {
-    if (isNaN(article_id)) {
-        return Promise.reject({ status: 400, msg: "Invalid article_id" });
-    }
-
     return db.query(`SELECT * FROM articles WHERE article_id = $1;`, [article_id])
         .then(({ rows }) => {
             if (rows.length === 0) {
                 return Promise.reject({ status: 404, msg: "Article not found" });
             }
-
-            return db.query(
-                `SELECT comment_id, votes, created_at, author, body, article_id
-                FROM comments
-                WHERE article_id = $1
-                ORDER BY created_at DESC;`,
-                [article_id]
-            );
         })
-        .then(({ rows }) => {
-            return rows;
+        .then(() => {
+            return db.query(`SELECT * FROM comments WHERE article_id = $1;`, [article_id])
+                .then(({ rows }) => rows);
         });
 }
 
-module.exports = { fetchCommentsByArticleId }
+function insertCommentByArticleId(article_id, username, body) {
+    return db.query(
+        `INSERT INTO comments (article_id, author, body, votes, created_at) 
+         VALUES ($1, $2, $3, 0, NOW()) 
+         RETURNING *;`,
+        [article_id, username, body]
+    )
+    .then(({ rows }) => {
+        return rows[0];
+    });
+};
+
+module.exports = { fetchCommentsByArticleId, insertCommentByArticleId }
