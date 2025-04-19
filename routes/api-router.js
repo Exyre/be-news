63124e4f71684246ab7e46de581f15c19e47cb7f
Exyre <munirv97@gmail.com>
2025-04-19 17:45:17 +0100

    Cleaned up seeding route

--- routes/api-router.js
+++ routes/api-router.js
@@ -1,12 +1,9 @@
+
 const express = require("express");
 const apiRouter = express.Router();
-const { getApiEndpoints } = require("../Controllers/apiController");
-const { seedDatabase } = require('../Controllers/db.controller');
 
-// Only add the seed route if we're not in test environment
-if (process.env.NODE_ENV !== 'test') {
-  apiRouter.post('/seed', seedDatabase);
-}
+
+const { getApiEndpoints } = require("../Controllers/apiController");
 
 apiRouter.get("/", getApiEndpoints);
 
